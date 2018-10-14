dadf77834e19d961509d4f6a7957728391e9b6ae
rahulkumar082 <rahulkr9622@gmail.com>
2018-10-14 19:31:14 +0530

    Framed the web page according to the question

--- Counter/index.js
+++ Counter/index.js
@@ -1,12 +1,8 @@
 function checkCounter(){
     let text = document.getElementById("txt").value;
     let words = text.split(' ').length;
-    document.getElementById('word').innerHTML = "Number of Words: "+(words+1);
     let lines = text.split('\n').length-1;
-    document.getElementById('lines').innerHTML = "Number of Lines: "+lines;
-    //console.log("number of lines: "+ lines);
     let char = text.split('').length;
     char = char-(lines+words-1);
-    document.getElementById('char').innerHTML = "Number of Characters: "+char;
-    //console.log("number of chars: "+(char-lines-words+1));
+    alert("\nNumber of Words : "+(words-1)+"\nNumber of Characters : "+char+"\nNumber of Lines: "+lines);
 }
