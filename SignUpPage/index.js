fbf294522c1d2b0b03ec1f560733ea9ba760bc3c
rahulkumar082 <rahulkr9622@gmail.com>
2018-10-14 23:57:36 +0530

    Added modules for part A

--- SignUpPage/index.js
+++ SignUpPage/index.js
@@ -3,12 +3,14 @@ function checkfname(){
     var fname = document.getElementById("firstName").value;
     if((/^[0-9]+$/).test(fname)){
         alert("Warning : Please Enter alphabets only");
+        document.getElementById("firstName").innerHTML="";
     }    
 }
 function checklname(){
     var lname = document.getElementById("lastName").value;
     if((/^[0-9]+$/).test(lname)){
         alert("Warning : Please Enter alphabets only");
+        document.getElementById("lasttName").innerHTML="";
     }    
 }
 function dateTake(){
@@ -16,10 +18,14 @@ function dateTake(){
 }
 function mobNum(){
     var mob = document.getElementById("mobileNum").value;
-    if(mobileNum.length>10)
+    if(mobileNum.length>10){
         alert("Warning: Wrong Mobile number");
-    if((/^[a-zA-Z]+$/).test(mob))
+        document.getElementById("mobNum").innerHTML="";
+    }
+    if((/^[a-zA-Z]+$/).test(mob)){
         alert("Warning: Alphabets in Mobile Number")
+        document.getElementById("mobNum").innerHTML="";
+    }
 }
 
 function submitThis(){
@@ -27,7 +33,9 @@ function submitThis(){
     var email = document.getElementById("emailId").value;
     if((email.includes('co')||email.includes('com')||email.includes('@')))
         alert("Submitted");
-    else
+    else{
     alert("Incorrect Email Address");
+    document.getElementById("emailId").innerHTML="";
+}
 
 }
